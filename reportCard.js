aeaee957cdb32731c11e3a60869367f56081dcb3
Sumit Kumar <112721086+hySumit@users.noreply.github.com>
2023-11-25 22:05:42 +0530

    Update reportCard.js

--- reportCard.js
+++ reportCard.js
@@ -1,19 +1,19 @@
-function Student(name, roll_no, classVal, section, marks_of_5_subjects) {
+function Student(name, roll_no, classVal, section, marks) {
     this.name = name;
     this.roll_no = roll_no;
     this.classVal = classVal;
     this.section = section;
-    this.marks_of_5_subjects = marks_of_5_subjects;
+    this.marks = marks;
 
-    this.printTop3Subjects = function () {
-        const subjects = Object.keys(this.marks_of_5_subjects);
-        const top3Subjects = subjects.sort((a, b) => this.marks_of_5_subjects[b] - this.marks_of_5_subjects[a]).slice(0, 3);
+    this.printSubs = function () {
+        const subjects = Object.keys(this.marks);
+        const top3Subjects = subjects.sort((a, b) => this.marks[b] - this.marks[a]).slice(0, 3);
         console.log('Top Three Subjects:', top3Subjects.join(', '));
     };
 
     this.printReportCard = function () {
-        const totalMarks = Object.values(this.marks_of_5_subjects).reduce((total, mark) => total + mark, 0);
-        const percentage = (totalMarks / (Object.keys(this.marks_of_5_subjects).length * 100)) * 100;
+        const totalMarks = Object.values(this.marks).reduce((total, mark) => total + mark, 0);
+        const percentage = (totalMarks / (Object.keys(this.marks).length * 100)) * 100;
 
         console.log('+---------------------------+');
         console.log('|        REPORT CARD       |');
@@ -23,8 +23,8 @@ function Student(name, roll_no, classVal, section, marks_of_5_subjects) {
         console.log(`| Class    - ${this.classVal}              `);
         console.log(`| Section  - ${this.section}             `);
 
-        for (const subject in this.marks_of_5_subjects) {
-            console.log(`| ${subject.charAt(0).toUpperCase() + subject.replace('_', ' ').slice(1)} - ${this.marks_of_5_subjects[subject]}`);
+        for (const subject in this.marks) {
+            console.log(`| ${subject.charAt(0).toUpperCase() + subject.replace('_', ' ').slice(1)} - ${this.marks[subject]}`);
         }
 
         console.log(`| Percentage - ${percentage.toFixed(1)} %`);
@@ -40,5 +40,5 @@ const student = new Student('Sumit', 16, 'S', 'D', {
     hindi: 37
 });
 
-student.printTop3Subjects();
+student.printSubs();
 student.printReportCard();
